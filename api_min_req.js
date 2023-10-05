d0b2d97430ff828c03f7dabaa08c46cb636cb8e8
JoakimJobloop <Joakim@jobloop.no>
2023-10-05 14:42:56 +0200

    *

--- api_min_req.js
+++ api_min_req.js
@@ -5,6 +5,7 @@ const fetchJoke = async () => {
   const response = await fetch("https://api.chucknorris.io/jokes/random");
 
   const data = await response.json();
+  console.log(data)
   jokeText.textContent = data.value;
 };
 
